ab553fad9777e5106ef8d95870fbc6267f094e4a
Dragon-23 <72811707+Dragon-23@users.noreply.github.com>
2021-05-13 19:38:13 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+function draw()
+{
+image(img, 0,0,600, 450);
+fill("red");
+text("Dog", 60,70);
+noFill();
+stroke("red");
+rect(30,30, 280,400);
+fill("red");
+text("Cat", 350, 70);
+noFill();
+rect(340, 30, 400, 400);
+}
+
+function preload()
+{
+    img = loadImage("dog_cat.jpg");
+
+}
+
+function setup()
+{
+    canvas = createCanvas(600, 450);
+    canvas.position(325, 125);
+    
+    
+}
