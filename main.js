2e89aaea4ddc8fd1e9848110ec2dd35b28743b4f
Dragon-23 <72811707+Dragon-23@users.noreply.github.com>
2021-05-15 14:31:23 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+img="";
+status="";
 function draw()
 {
 image(img, 0,0,600, 450);
@@ -22,6 +24,27 @@ function setup()
 {
     canvas = createCanvas(600, 450);
     canvas.position(325, 125);
+    objectDetection = ml5.objectDetector("cocossd", modelLoaded);
+   document.getElementById("status").innerHTML = "Status: Detecting Object";
+
     
     
+}
+
+function modelLoaded()
+{
+    console.log("Model Loaded!");
+    status = true;
+    objectDetection.detect(img, gotResult);
+
+}
+
+
+function gotResult(error, results)
+{
+    if(error)
+    {
+        console.error(error);
+    }
+    console.log(results);
 }
